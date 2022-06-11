7f2ee9d4bad3eedcd5bdc2eb0867ff3d74d69f5f
daihuan1 <daihuan1@internal.ths.com.cn>
2022-06-11 23:44:13 +0800

    提交功能

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <layout></layout>
+  </div>
+</template>
+
+<script>
+import Layout from '@/layout'
+export default {
+  components: {
+    Layout
+  }
+}
+</script>
